fb009667fb2b4018416ee557be970631598d193c
kubaz45624 <jakub.zielinski45624@gmail.com>
2021-03-25 19:26:28 +0100

    Adding serviceDataRange repository, service, and model. Adding add dataRange to serviceDataRangeService

---
+++ src/main/java/com/example/dorywcza/repository/ServiceDataRangeRepository.java
@@ -0,0 +1,7 @@
+package com.example.dorywcza.repository;
+
+import com.example.dorywcza.model.service_offer.ServiceDateRange;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ServiceDataRangeRepository extends JpaRepository<ServiceDateRange, Integer> {
+}
