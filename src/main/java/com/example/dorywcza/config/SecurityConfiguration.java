5a21904a8fdcc9fbd7fef29fb346231a5b738a67
Joanna Rzeszotko-Doniec <rivienne8@gmail.com>
2021-03-26 16:33:47 +0100

    refactor

--- src/main/java/com/example/dorywcza/config/SecurityConfiguration.java
+++ src/main/java/com/example/dorywcza/config/SecurityConfiguration.java
@@ -1,4 +1,4 @@
-package com.example.dorywcza.model.config;
+package com.example.dorywcza.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
