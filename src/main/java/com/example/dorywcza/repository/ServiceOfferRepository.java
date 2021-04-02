2065e65cab01e73c089673403ce8f8c43424d324
kubaz45624 <jakub.zielinski45624@gmail.com>
2021-04-02 14:55:37 +0200

    Changing service controller to DTO

--- src/main/java/com/example/dorywcza/repository/ServiceOfferRepository.java
+++ src/main/java/com/example/dorywcza/repository/ServiceOfferRepository.java
@@ -6,8 +6,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface ServiceOfferRepository extends JpaRepository<ServiceOffer, Long> {
-
-//    List<ServiceOffer> findAllByOrderByServiceDateRangeStartDateAsc();
-//
-//    List<ServiceOffer> findAllByServiceScheduleMondayAfternoon(boolean check);
 }
