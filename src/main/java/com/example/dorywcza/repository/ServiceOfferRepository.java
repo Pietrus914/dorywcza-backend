440116f8dc0ed14f32f31d840f12698f5797490b
kubaz45624 <jakub.zielinski45624@gmail.com>
2021-04-08 18:20:34 +0200

    Adding search method matching job offer

--- src/main/java/com/example/dorywcza/repository/ServiceOfferRepository.java
+++ src/main/java/com/example/dorywcza/repository/ServiceOfferRepository.java
@@ -4,6 +4,8 @@ import com.example.dorywcza.model.service_offer.ServiceOffer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Date;
 
 public interface ServiceOfferRepository extends JpaRepository<ServiceOffer, Long> {
+    List<ServiceOffer> findByIndustryIdAndOfferLocation_xPositionAndOfferLocation_yPositionAndDateRangeStartDateLessThanEqualAndHasExperienceOrHasExperienceIsTrue(Long industryId, double xPosition, double yPosition, Date endDate, boolean hasExperience);
 }
