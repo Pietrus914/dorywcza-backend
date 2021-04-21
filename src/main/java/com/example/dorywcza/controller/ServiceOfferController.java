3669dc26db246a2b4a1301579511a66c53691f2e
kubaz45624 <jakub.zielinski45624@gmail.com>
2021-04-21 12:30:06 +0200

    adding crossOrigin

--- src/main/java/com/example/dorywcza/controller/ServiceOfferController.java
+++ src/main/java/com/example/dorywcza/controller/ServiceOfferController.java
@@ -8,6 +8,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class ServiceOfferController {
     private final ServiceOfferService serviceOfferService;
 
