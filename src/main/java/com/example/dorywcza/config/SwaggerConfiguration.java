974fa7d1badf625b5f201e9ee91d71abbadbee2c
Joanna Rzeszotko-Doniec <rivienne8@gmail.com>
2021-04-29 13:40:13 +0200

    adding swagger dependency and configuration

---
+++ src/main/java/com/example/dorywcza/config/SwaggerConfiguration.java
@@ -0,0 +1,40 @@
+package com.example.dorywcza.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.data.rest.configuration.SpringDataRestConfiguration;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+@Import(SpringDataRestConfiguration.class)
+public class SwaggerConfiguration {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Dorywcza.pl API")
+                .description("Welcome! You can find here instructions how to use API to get data from resources. Enjoy!")
+                .termsOfServiceUrl("")
+                .version("1.0")
+                .contact(new Contact("Admin", "http://localhost:8080/dorywcza/docs", "admin@dorywcza.pl"))
+                .build();
+    }
+}
